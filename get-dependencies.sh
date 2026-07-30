a3ee5c4c2bbf1300af5cdf3e3cc7bd252dcd7c5b
Samueru-sama <xdglawyer@outlook.com>
2026-07-30 02:33:59 -0400

    try to support `aarch64`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,14 +6,14 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm deepin-calculator kvantum lxqt-qtplugin qt6ct
+pacman -Syu --noconfirm kvantum lxqt-qtplugin qt6ct
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package --archlinux-pkg deepin-calculator
 
 # If the application needs to be manually built that has to be done down here
 
